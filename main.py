72fbd024498dd9498507ffb16c16b94f9fe433ed
hannesheimbach3004 <h.heimbach@mci4me.at>
2026-05-21 21:05:38 +0200

    erste version, bis zum bilder implementieren

---
+++ main.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from PIL import Image
+import read_data
+
+person_data = read_data.load_person_data()
+personenliste = read_data.get_person_list(person_data)
+
+# Eine Überschrift der ersten Ebene
+st.write("# EKG APP")
+
+# Eine Überschrift der zweiten Ebene
+st.write("## Versuchsperson auswählen")
+
+# Eine Auswahlbox
+current_user = st.selectbox("Patienten", options=personenliste, key="sbVersuchsperson")
+
+currentpicturepath = read_data.picture_path(
+    current_user
+)  # den picturepath aus dem dicitonary auslesen
+
+# Laden eines Bilds
+image = Image.open(currentpicturepath)
+# Anzeigen eines Bilds mit Caption
+st.image(image, caption=current_user)
+
+
+if __name__ == "__main__":
+    person_dict = read_data.load_person_data()
+    person_names = read_data.get_person_list(person_dict)
+    print(person_names)
+    print(current_user)
+    currentuserdata = read_data.find_person_data_by_name(current_user)
+    print(currentuserdata)
+    currentpicturepath = read_data.picture_path(current_user)
+    print(currentpicturepath)  ###
---
+++ read_data.py
@@ -0,0 +1,47 @@
+import json
+
+
+def load_person_data():
+    """A Function that knows where the person database is and returns a dictionary with the persons"""
+    file = open("data/person_db.json")
+    person_data = json.load(file)
+    return person_data
+
+
+def get_person_list(person_data):
+    persons_list = []
+    for pers_dict in person_data:
+        person_dict = pers_dict
+        persons_list.append(pers_dict["lastname"] + ", " + person_dict["firstname"])
+    return persons_list
+
+
+def find_person_data_by_name(suchstring):
+
+    person_data = load_person_data()
+
+    if suchstring == "None":
+        return {}
+
+    two_names = suchstring.split(", ")
+    vorname = two_names[1]
+    nachname = two_names[0]
+
+    for eintrag in person_data:
+        if eintrag["lastname"] == nachname and eintrag["firstname"] == vorname:
+            return eintrag
+        else:
+            return {}
+
+
+def picture_path(suchstring):
+    current_person_dict = find_person_data_by_name(suchstring)
+
+    current_picture_path = current_person_dict["picture_path"]
+
+    return current_picture_path
+
+
+if __name__ == "__main__":
+    find_person_data_by_name("Huber, Julian")
+    picture_path("Huber, Julian")
